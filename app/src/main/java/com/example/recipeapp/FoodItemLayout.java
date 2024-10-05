7c5e9a146c6c1a7727aaa9dbf389a75304da17c0
sujikaran07 <153360996+sujikaran07@users.noreply.github.com>
2024-10-05 12:19:36 +0530

    Update from IM/2021/119

---
+++ app/src/main/java/com/example/recipeapp/FoodItemLayout.java
@@ -0,0 +1,14 @@
+package com.example.recipeapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FoodItemLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the content view to your layout file
+        setContentView(R.layout.food_item_layout);
+    }
+}
